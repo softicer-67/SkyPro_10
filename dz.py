d91aa4b0550f760d765a2de39ea4f54e22f4e63f
Yuri <57116431+softicer-67@users.noreply.github.com>
2022-09-20 16:03:15 +0500

    Add files via upload

--- dz.py
+++ dz.py
@@ -47,24 +47,24 @@ def get_question():
 
 def main():
     count = 0
+    good_answer = 0
     print("Игра начинается!")
     while count != 5:
 
         quest = get_question()
         p = Question(quest[0], quest[1], quest[2])
-
         my_points = p.get_points()
+
         p.build_question(quest[0], quest[1])
         user_response = input('Ответ: ')
         if user_response == str(quest[2]):
             p.build_positive_feedback(10)
-            good = p.correct_answer
-
+            good_answer += 1
         else:
             p.build_negative_feedback(quest[2])
         count += 1
 
-    print(f"Вот и все!\nОтвечено на {good} вопроса из {count}\nНабранно {my_points} баллов")
+    print(f"Вот и все!\nОтвечено на {good_answer} вопроса из {count}\nНабранно {my_points} баллов")
 
 
 if __name__ == '__main__':
